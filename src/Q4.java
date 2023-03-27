7a55f372d799158634e2e2c1fd29c47625c0f423
DK <dongwook5478@gmail.com>
2023-03-27 10:08:53 +0900

    Q4 add scanner

--- src/Q4.java
+++ src/Q4.java
@@ -4,8 +4,10 @@
     3-27-2023
     Dounguk Kim
  */
+import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args){
-
+        Scanner sc=new Scanner(System.in);
+        int getNum=sc.nextInt();
     }
 }

c55aff652c115d24b26f5345abadb47e55e49f9f
DK <dongwook5478@gmail.com>
2023-03-27 14:35:37 +0900

    Q4 70%

--- src/Q4.java
+++ src/Q4.java
@@ -4,10 +4,40 @@
     3-27-2023
     Dounguk Kim
  */
+
 import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int getNum=sc.nextInt();
+        int[] getNum=new int[9];
+        int[] sum=new int[7];
+        sum[6]=0;
+
+
+//        input
+        for (int i = 0;i<9;i++){
+            System.out.println("put " + (9-i) + " more numbers");
+            getNum[i]=sc.nextInt();
+            sum[6]+=getNum[i];
+        }
+
+//        calc.
+
+
+
+//          output
+        for (int i =0;i<9;i++){
+            if(i%3==0){
+                System.out.println(" ");
+            }
+            System.out.print("   "+getNum[i]+"   ");
+        }
+        System.out.print("   "+sum[2]+"   ");
+        System.out.println("");
+        System.out.println("   "+sum[3]+"   "+"   "+sum[4]+"   "+"   "+sum[5]+"   "+"   "+sum[6]+"   ");
+
+
+
+
     }
 }

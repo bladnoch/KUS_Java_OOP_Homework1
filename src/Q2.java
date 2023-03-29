2c5bd888c9907d86b6fe1695b885d1d3f93d7e7f
DK <dongwook5478@gmail.com>
2023-03-30 00:02:17 +0900

    final

--- src/Q2.java
+++ src/Q2.java
@@ -1,27 +1,26 @@
 /*
         Q2.java
-        Shows today's year,month, date, and season in GUI
+        Shows today's year,month, date, season, and recommend three seasonal food
         3-27-2023
         Dounguk Kim
  */
 import javax.swing.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Q2 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException {
         int getMonth;
         String season;
+        String[][] food={{"딸기", "한라봉", "장어"},{"참외", "복숭아", "포도"},{"귤","토마토", "유자"},{"한라봉"," 석류", "사과"}};
+        String[] sfood=new String[3];
         Date today=new Date();
         SimpleDateFormat date=new SimpleDateFormat("yyyy-MM-dd");
 
 
-
 //        input
-        Scanner sc= new Scanner(System.in);
-        System.out.println("기다리는 시즌의 날짜를 입력해 주세요(yyyy-MM-dd)");
-        String newDate=sc.next();
 
 
 //        calc.
@@ -29,21 +28,23 @@ public class Q2 {
 
         if (getMonth<=5 && getMonth>=3){
             season="봄입니다.";
+            sfood=food[0];
         }
         else if (getMonth<=8 && getMonth>=6){
             season="여름입니다.";
+            sfood=food[1];
         }
         else if (getMonth<=9 && getMonth>=11){
             season="가을입니다." ;
+            sfood=food[2];
         }
         else{
             season="겨울입니다.";
+            sfood=food[3];
         }
 
-//        String newDate=
 
 //        output
-        JOptionPane.showMessageDialog(null,date.format(today)+"\n"+season);
-
+        JOptionPane.showMessageDialog(null,date.format(today)+"\n"+"지금은 "+season+" 입니다.\n"+sfood[0]+", "+sfood[1]+", "+sfood[2]+"가 제철식품 입니다.");
     }
 }

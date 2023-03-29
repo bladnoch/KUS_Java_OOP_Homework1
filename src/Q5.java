d92f76e6ca2f45fae4f407f50d8f03fcdd24674f
DK <dongwook5478@gmail.com>
2023-03-29 23:15:17 +0900

    edit Q5

--- src/Q5.java
+++ src/Q5.java
@@ -12,7 +12,7 @@ public class Q5 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int[] money2= new int[9];
-        String[] money= {"50000","10000","5000","1000","500","100","50", "10","1"};
+        int[] money= {50000,10000,5000,1000,500,100,50, 10,1};
 
 
 //      input
@@ -20,29 +20,11 @@ public class Q5 {
         money2[8]=sc.nextInt();
 
 //      calc.
-        money2[0]=money2[8]/50000;
-        money2[8]-=money2[0]*50000;
 
-        money2[1]=money2[8]/10000;
-        money2[8]-=money2[1]*10000;
-
-        money2[2]=money2[8]/5000;
-        money2[8]-=money2[3]*5000;
-
-        money2[3]=money2[8]/1000;
-        money2[8]-=money2[3]*1000;
-
-        money2[4]=money2[8]/500;
-        money2[8]-=money2[4]*500;
-
-        money2[5]=money2[8]/100;
-        money2[8]-=money2[5]*100;
-
-        money2[6]=money2[8]/50;
-        money2[8]-=money2[6]*50;
-
-        money2[7]=money2[8]/10;
-        money2[8]-=money2[7]*10;
+        for (int i=0;i<money.length;i++){
+            money2[i]=money2[8]/money[i];
+            money2[8]-=money2[i]*money[i];
+        }
 
 
 //      output
